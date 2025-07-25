cba944e8146eb161421d6742c1ae21943e83ff84
FelipeM <almeida.felipejs@gmail.com>
2025-07-25 10:50:19 -0300

    desafio finalizado

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let inputUser = document.getElementById('amigo').value;
+    if (inputUser == '') {
+        alert('Por favor, insira um nome.');
+        return
+    }
+    amigos.push(inputUser);
+    limparCampo();
+    listarAmigos();
+}
+function limparCampo() {
+    inputUser = document.getElementById('amigo');
+    inputUser.value = '';
+}
+
+function listarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    amigos.forEach(amigo => {
+        let novoAmigo = document.createElement('li');
+        novoAmigo.innerText = amigo;
+        listaAmigos.appendChild(novoAmigo);
+    });
+}
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Adicione amigos para começar o sorteio.');
+        return
+    }
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let resultado = document.getElementById('resultado');
+    resultado.innerText = amigos[indiceSorteado];
+}
